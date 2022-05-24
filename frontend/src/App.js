6f44f7a7d3c93bff97915298cfa23d4f3ebbbf77
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2022-05-24 19:12:16 +0530

    added initial countdown functionality

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import Progress from "./components/Progress";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(100);
+  const [isActive, setIsActive] = useState(false);
+  const [pcount, setPcount] = useState(5);
+
+  useEffect(() => {
+    let timer = null;
+    if (isActive) {
+      timer = setInterval(() => {
+        setCount((prev) => prev - 5);
+      }, 300);
+    } else {
+      clearInterval(timer);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isActive]);
+
+  const timerStarter = () => {
+    setIsActive(true);
+  };
+
+  const resetter = () => {
+    setCount(100);
+  };
+
+  const stopHandler = () => {
+    setIsActive(false);
+  };
+
+  if (count <= -5) {
+    setIsActive(false);
+    setCount(100);
+    setPcount((prev) => prev - 1);
+  }
+
+  return (
+    <>
+      <h1>Tracker App {pcount} </h1>
+      <button onClick={timerStarter}>Start</button>
+      <button onClick={stopHandler}>Stop</button>
+      <button onClick={resetter}>Reset</button>
+      <Progress value={count} />
+    </>
+  );
+}
+
+export default App;
